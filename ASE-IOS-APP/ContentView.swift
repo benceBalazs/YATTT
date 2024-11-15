c6e5ab1adc93abc92dd918836af08613a36b337f
Laura_LL <lauralik@edu.aau.at>
2024-11-15 12:15:50 +0100

    Initial Commit

---
+++ ASE-IOS-APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASE-IOS-APP
+//
+//  Created by Laura likar on 15.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ASE-IOS-APPTests/ASE_IOS_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  ASE_IOS_APPTests.swift
+//  ASE-IOS-APPTests
+//
+//  Created by Laura likar on 15.11.24.
+//
+
+import Testing
+@testable import ASE_IOS_APP
+
+struct ASE_IOS_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
