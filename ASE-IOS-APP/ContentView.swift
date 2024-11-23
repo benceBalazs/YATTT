ef070aa7efbf6d56e637bf0f39c42986902c4cfd
LauraL000 <lauralik@edu.aau.at>
2024-11-23 14:59:12 +0100

    test

--- ASE-IOS-APP/ContentView.swift
+++ ASE-IOS-APP/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             HomeView()
                 .tabItem {
                     Image(systemName: "house")
-                    Text("Home")
+                    Text("home")
                 }
             
             // Settings Tab
