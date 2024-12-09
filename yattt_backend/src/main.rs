cef69a545e373a48872d391fcbaa39eb02cad719
HafiTheCat <f2hafner@edu.aau.at>
2024-12-10 00:51:57 +0100

    Get python_ds_api_key from .env

--- yattt_backend/src/main.rs
+++ yattt_backend/src/main.rs
@@ -9,12 +9,24 @@ use utoipa::{
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_scalar::{Scalar, Servable as ScalarServable};
 
+// The API key is loaded from the environment variable PYTHON_SERVICE_API_KEY
+static PYTHON_SERVICE_API_KEY: std::sync::LazyLock<String> = std::sync::LazyLock::new(|| {
+    match std::env::var("PYTHON_SERVICE_API_KEY") {
+        Ok(api_key) if !(api_key.is_empty()) => api_key,
+        _ => panic!("PYTHON_SERVICE_API_KEY must be set and cannot be empty!"),
+    }
+});
+
 const YATTT_TAG: &str = "yatt";
 const API_VERSIONING: &str = "v5";
 const APPLICATION_PORT: u16 = 8080;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+
+    // load the env variables from .env
+    dotenvy::dotenv().ok();
+
     #[derive(OpenApi)]
     #[openapi(
         tags(
