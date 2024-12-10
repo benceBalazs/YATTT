58a112256e81af5ec1cd9362d201e759069ce54b
HafiTheCat <f2hafner@edu.aau.at>
2024-12-10 02:23:37 +0100

    Restructured Routes for better extendability

--- yattt_backend/src/main.rs
+++ yattt_backend/src/main.rs
@@ -1,3 +1,5 @@
+mod routes;
+
 use std::net::{Ipv4Addr, SocketAddr};
 
 use std::io::Error;
@@ -20,6 +22,7 @@ static PYTHON_SERVICE_API_KEY: std::sync::LazyLock<String> = std::sync::LazyLock
 const YATTT_TAG: &str = "yatt";
 const API_VERSIONING: &str = "v5";
 const APPLICATION_PORT: u16 = 8080;
+const DOCS_ROUTE: &str = "/docs";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -35,15 +38,26 @@ async fn main() -> Result<(), Error> {
     )]
     struct ApiDoc;
 
+    // generate the documentation router
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
-        .nest(format!("/api/{API_VERSIONING}/").as_str(), yattt::router())
+        .nest(DOCS_ROUTE, yattt::router())
         .split_for_parts();
 
-    let router = router.merge(Scalar::with_url("/scalar", api));
+    // define the `/v1` router
+    let v1_routes = axum::Router::new()
+        .route("/", axum::routing::get(crate::routes::root::root_handler))
+        .merge(router)
+        .merge(Scalar::with_url(DOCS_ROUTE, api));
+
+    // define the `/api` router and nest `/v1` under `/api`
+    let api_version_routes = axum::Router::new().nest("/v1", v1_routes);
+
+    let app = axum::Router::new().nest("/api", api_version_routes);
 
     let address = SocketAddr::from((Ipv4Addr::LOCALHOST, APPLICATION_PORT));
     let listener = TcpListener::bind(&address).await?;
-    axum::serve(listener, router.into_make_service()).await
+
+    axum::serve(listener, app.into_make_service()).await
 }
 
 mod yattt {
