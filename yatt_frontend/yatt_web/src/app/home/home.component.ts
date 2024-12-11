0b5235de9ee3d5b53a76ce65ab4112e7cad6ca2f
verenarath <verenarath@edu.aau.at>
2024-12-11 18:06:15 +0100

    added homepage

--- yatt_frontend/yatt_web/src/app/home/home.component.ts
+++ yatt_frontend/yatt_web/src/app/home/home.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatTable,
+    MatHeaderCell,
+    MatHeaderRow,
+    MatRow,
+    MatCellDef,
+    MatCell,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatSort,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  displayedColumns: string[] = ['checkIn', 'checkOut', 'duration', 'card', 'course'];
 
+  dataSource = [
+    { checkIn: '08:00 AM', checkOut: '10:00 AM', duration: '2h 0m', card: '489235', course: 'Advanced Software Engineering' },
+    { checkIn: '10:15 AM', checkOut: '12:15 PM', duration: '2h 0m', card: '1234123', course: 'Security Engineering' },
+    { checkIn: '01:00 PM', checkOut: '03:30 PM', duration: '2h 30m', card: '42356425', course: 'Technology Assesment' },
+    { checkIn: '03:45 PM', checkOut: '05:15 PM', duration: '1h 30m', card: '1234123', course: 'Distributed Computing Infrastructures' },
+    { checkIn: '06:00 PM', checkOut: '08:00 PM', duration: '2h 0m', card: '505132412', course: 'Introduction to Artificial Intelligence' }
+  ]
 }
+
