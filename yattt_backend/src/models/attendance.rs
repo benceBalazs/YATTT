2ac1c56e9e189ed0186145aff73dae14d48aaaee
HafiTheCat <f2hafner@edu.aau.at>
2024-12-12 01:27:02 +0100

    changed Attendance model
    - Options are unneeded for check_out_time and duration

--- yattt_backend/src/models/attendance.rs
+++ yattt_backend/src/models/attendance.rs
@@ -1,21 +1,21 @@
 use serde::{Deserialize, Serialize};
-use chrono::{DateTime, Utc}; // Use chrono for handling datetime
+use chrono::{DateTime, Utc};
+use surrealdb::sql::Thing; // Use chrono for handling datetime
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Attendance {
-    pub id: Option<String>, // SurrealDB will generate a record ID
+    pub id: Option<Thing>, // SurrealDB will generate a record ID
     pub user: String,       // Reference to User's ID
     pub device_id: String,
     pub check_in_time: DateTime<Utc>,
-    pub check_out_time: Option<DateTime<Utc>>, // Optional field for check-out time
-    pub duration: Option<f64>, // Duration in hours (optional)
+    pub check_out_time: DateTime<Utc>,
+    pub duration: f64,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AttendanceResponse {
-    pub user: String,       // Reference to User's ID
+pub struct AttendanceRequest {
+    pub tag_id: String,
     pub device_id: String,
     pub check_in_time: DateTime<Utc>,
-    pub check_out_time: Option<DateTime<Utc>>, // Optional field for check-out time
-    pub duration: Option<f64>, // Duration in hours (optional)
+    pub check_out_time: DateTime<Utc>,
+    pub duration: f64,
 }
