b6fd6d5e3c7c26cdecb29b87dba5604ca6d212ed
HafiTheCat <f2hafner@edu.aau.at>
2025-01-20 14:50:31 +0100

    defined cors restrictions

--- yattt_backend/src/routes/mod.rs
+++ yattt_backend/src/routes/mod.rs
@@ -3,12 +3,14 @@ pub mod auth;
 pub mod card;
 pub mod root;
 
+use crate::YatttAppState;
 use axum::{middleware, Extension, Router};
+use hyper::{header::{AUTHORIZATION, CONTENT_TYPE}, Method};
+use tower_http::cors::{Any, CorsLayer};
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_axum::routes;
 use utoipa_scalar::{Scalar, Servable as ScalarServable};
-use crate::YatttAppState;
 
 #[derive(utoipa::OpenApi)]
 #[openapi(
@@ -47,14 +49,17 @@ pub fn setup_routes(state: crate::YatttAppState) -> Router {
 
     let (protected_attendance_router, attendance_api) =
         OpenApiRouter::<crate::YatttAppState>::with_openapi(ApiDoc::openapi())
-            .routes(routes!(crate::routes::attendance::attendance_retrieve_handler))
+            .routes(routes!(
+                crate::routes::attendance::attendance_retrieve_handler
+            ))
             .layer(middleware::from_fn_with_state(
                 state.clone(),
                 crate::routes::auth::authorization_layer,
             ))
-            .routes(routes!(crate::routes::attendance::attendance_create_handler))
+            .routes(routes!(
+                crate::routes::attendance::attendance_create_handler
+            ))
             .split_for_parts();
-
 
     let merged_router = axum::Router::new()
         .merge(unprotected_root_router)
@@ -78,5 +83,16 @@ pub fn setup_routes(state: crate::YatttAppState) -> Router {
     // define the `/api` router and nest `/v1` under `/api`
     let api_version_routes = axum::Router::new().nest("/v1", v1_routes).with_state(state);
 
+    let api_version_routes = api_version_routes.layer(
+        CorsLayer::new().allow_origin(Any).allow_methods(vec![
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+        ]).allow_headers([
+            AUTHORIZATION, // Explicitly allow the Authorization header
+            CONTENT_TYPE,  // Allow other headers like Content-Type if needed
+        ]),
+    );
     axum::Router::new().nest("/api", api_version_routes)
 }
