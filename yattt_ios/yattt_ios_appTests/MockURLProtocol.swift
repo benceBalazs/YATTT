8ed563d054256ff778f5544072de944c21a78365
LauraL000 <lauralik@edu.aau.at>
2025-01-20 14:27:11 +0100

    add test

---
+++ yattt_ios/yattt_ios_appTests/MockURLProtocol.swift
@@ -0,0 +1,25 @@
+//Mocking
+class MockURLProtocol: URLProtocol {
+    static var responseData: Data?
+    static var responseError: Error?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        if let responseData = MockURLProtocol.responseData {
+            self.client?.urlProtocol(self, didLoad: responseData)
+        }
+        if let responseError = MockURLProtocol.responseError {
+            self.client?.urlProtocol(self, didFailWithError: responseError)
+        }
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    override func stopLoading() {}
+}
