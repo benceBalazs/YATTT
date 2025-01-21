f913f29692ae9cc9bf781f239b3732aa471d06da
verenarath <verenarath@edu.aau.at>
2025-01-21 21:24:49 +0100

    removed navBar from LoginPage

--- yatt_frontend/yatt_web/src/app/app.component.ts
+++ yatt_frontend/yatt_web/src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { Component, OnInit } from '@angular/core';
+import {Router, NavigationEnd, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    NavbarComponent,
+    RouterOutlet,
+    NgIf
+  ],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'yattt_web';
+export class AppComponent implements OnInit {
+  isLoginRoute = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('Current URL:', event.url);
+        this.isLoginRoute = event.url === '/login';
+        console.log('isLoginRoute:', this.isLoginRoute);
+      }
+    });
+  }
 }
