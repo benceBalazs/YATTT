ce30a38c1c2a11bd16408b50b4154926760b2398
verenarath <verenarath@edu.aau.at>
2025-01-21 21:00:02 +0100

    added interceptor for bearerToken

--- yatt_frontend/yatt_web/src/app/data-api.service.ts
+++ yatt_frontend/yatt_web/src/app/data-api.service.ts
@@ -20,60 +20,24 @@ export class DataApiService {
     return this.http.post(`${this.baseUrl}/auth/register`, { username, password });
   }
 
-  refreshToken(): Observable<any> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.post(`${this.baseUrl}/`, {}, { headers });
-  }
-
-  getHeaders(): HttpHeaders {
-    const token = localStorage.getItem('access_token');
-    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  }
-
   getAttendances(): Observable<any> {
-    const headers = {
-      Authorization:
-        'Bearer ' +
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzc1NTY4NzAsImlhdCI6MTczNzQ3MDQ3MCwidXNlcl9pZCI6ImRwY2l3azVhcmR0OHpxeWV1aG9tIn0.Pv-XUn9sPsmngbLW0ltxqqXUe1q8XopgxLvV5emqPLs',
-    };
-    return this.http.get<Observable<any>>(this.baseUrl + '/attendances', {
-      headers,
-    });
+    return this.http.get<Observable<any>>(this.baseUrl + '/attendances');
   }
 
   getCards(): Observable<GetCardResponse> {
-    const headers = {
-      Authorization:
-        'Bearer ' +
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzc1NTY4NzAsImlhdCI6MTczNzQ3MDQ3MCwidXNlcl9pZCI6ImRwY2l3azVhcmR0OHpxeWV1aG9tIn0.Pv-XUn9sPsmngbLW0ltxqqXUe1q8XopgxLvV5emqPLs',
-    };
-    return this.http.get<GetCardResponse>(this.baseUrl + '/cards', { headers });
+
+    return this.http.get<GetCardResponse>(this.baseUrl + '/cards');
   }
   addCard(card: Card): Observable<any> {
-    const headers = {
-      Authorization:
-        'Bearer ' +
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzc1NTY4NzAsImlhdCI6MTczNzQ3MDQ3MCwidXNlcl9pZCI6ImRwY2l3azVhcmR0OHpxeWV1aG9tIn0.Pv-XUn9sPsmngbLW0ltxqqXUe1q8XopgxLvV5emqPLs',
-    };
-    return this.http.post(this.baseUrl + '/cards', card, { headers });
+    return this.http.post(this.baseUrl + '/cards', card,);
   }
 
   updateCard(card_id: string, card: Card): Observable<any> {
-    const headers = {
-      Authorization:
-        'Bearer ' +
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzc1NTY4NzAsImlhdCI6MTczNzQ3MDQ3MCwidXNlcl9pZCI6ImRwY2l3azVhcmR0OHpxeWV1aG9tIn0.Pv-XUn9sPsmngbLW0ltxqqXUe1q8XopgxLvV5emqPLs',
-    };
-    return this.http.put(`${this.baseUrl}/cards/${card_id}`, card, {headers});
+
+    return this.http.put(`${this.baseUrl}/cards/${card_id}`, card);
   }
 
   deleteCard(card_id: string): Observable<any> {
-    const headers = {
-      Authorization:
-        'Bearer ' +
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzc1NTY4NzAsImlhdCI6MTczNzQ3MDQ3MCwidXNlcl9pZCI6ImRwY2l3azVhcmR0OHpxeWV1aG9tIn0.Pv-XUn9sPsmngbLW0ltxqqXUe1q8XopgxLvV5emqPLs',
-    };
-    return this.http.delete(`${this.baseUrl}/cards/${card_id}`, {headers});
+    return this.http.delete(`${this.baseUrl}/cards/${card_id}`);
   }
 }
