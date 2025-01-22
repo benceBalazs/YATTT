b3264ff2d8d3fadeb0af5a1e9efacf9d822cc0ae
verenarath <verenarath@edu.aau.at>
2025-01-22 17:31:20 +0100

    tableDesign cards

--- yatt_frontend/yatt_web/src/app/cards/cards.component.ts
+++ yatt_frontend/yatt_web/src/app/cards/cards.component.ts
@@ -1,8 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { DataApiService } from '../../services/data-api.service';
 import { Card, CardWithId } from '../project.model';
 import {FormsModule} from '@angular/forms';
 import {NgForOf, NgIf} from '@angular/common';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, Sort} from '@angular/material/sort';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from '@angular/material/table';
 
 @Component({
   selector: 'app-cards',
@@ -11,7 +22,19 @@ import {NgForOf, NgIf} from '@angular/common';
   imports: [
     FormsModule,
     NgIf,
-    NgForOf
+    NgForOf,
+    MatPaginator,
+    MatSort,
+    MatTable,
+    MatCell,
+    MatHeaderCell,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef
   ]
 })
 export class CardsComponent implements OnInit {
@@ -19,6 +42,11 @@ export class CardsComponent implements OnInit {
   cardForm: CardWithId = { card_name: '', tag_id: '' };
   showModal = false;
   isEdit = false;
+  displayedColumns: string[] = ['card_name', 'tag_id', 'actions'];
+  dataSource = new MatTableDataSource<CardWithId>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private dataApiService: DataApiService) {}
 
@@ -26,10 +54,29 @@ export class CardsComponent implements OnInit {
     this.fetchCards();
   }
 
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    console.log('Paginator:', this.paginator);
+    console.log('Sort:', this.sort);
+
+  }
+
+  sortCourse(sort: Sort) {}
+
   fetchCards(): void {
-    this.dataApiService.getCards().subscribe((cards) => {
-      this.cards = cards.cards;
-    });
+    this.dataApiService.getCards().subscribe(
+      (cards) => {
+        this.cards = cards.cards; // Assuming response contains a `cards` property
+        this.dataSource.data = this.cards;
+        this.dataSource.paginator = this.paginator; // Reassign after data load
+        this.dataSource.sort = this.sort;
+      },
+      (error) => {
+        console.error('Error fetching cards:', error);
+      }
+    );
   }
 
   openAddModal(): void {
