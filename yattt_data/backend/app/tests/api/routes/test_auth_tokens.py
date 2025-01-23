22e4f7005cd9ebcd7b5806de4296b93473bc0fbc
Philipp Fischbacher <ph.fischbacher@gmail.com>
2025-01-23 01:05:47 +0100

    add data service test pipe

--- yattt_data/backend/app/tests/api/routes/test_auth_tokens.py
+++ yattt_data/backend/app/tests/api/routes/test_auth_tokens.py
@@ -4,60 +4,61 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session
 
 from app.core.config import settings
-from app.tests.utils.item import create_random_item
+
+from backend.app.tests.utils.auth_token import create_random_auth_token
 
 
-def test_create_item(
+def test_create_auth_token(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
-    data = {"title": "Foo", "description": "Fighters"}
+    data = {"tag_id": "Foo", "device_id": "Fighters"}
     response = client.post(
-        f"{settings.API_V1_STR}/items/",
+        f"{settings.API_V1_STR}/auth-tokens/",
         headers=superuser_token_headers,
         json=data,
     )
     assert response.status_code == 200
     content = response.json()
-    assert content["title"] == data["title"]
-    assert content["description"] == data["description"]
+    assert content["tag_id"] == data["tag_id"]
+    assert content["device_id"] == data["device_id"]
     assert "id" in content
-    assert "owner_id" in content
+    assert "scanned_in" in content
 
 
-def test_read_item(
+def test_read_auth_token(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
-    item = create_random_item(db)
+    auth_token = create_random_auth_token(db)
     response = client.get(
-        f"{settings.API_V1_STR}/items/{item.id}",
+        f"{settings.API_V1_STR}/auth-tokens/{auth_token.id}",
         headers=superuser_token_headers,
     )
     assert response.status_code == 200
     content = response.json()
-    assert content["title"] == item.title
-    assert content["description"] == item.description
-    assert content["id"] == str(item.id)
-    assert content["owner_id"] == str(item.owner_id)
+    assert content["tag_id"] == auth_token.tag_id
+    assert content["device_id"] == auth_token.device_id
+    assert content["id"] == str(auth_token.id)
+    assert content["scanned_in"] == str(auth_token.scanned_in)
 
 
-def test_read_item_not_found(
+def test_read_auth_token_not_found(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
     response = client.get(
-        f"{settings.API_V1_STR}/items/{uuid.uuid4()}",
+        f"{settings.API_V1_STR}/auth-tokens/{uuid.uuid4()}",
         headers=superuser_token_headers,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == "Item not found"
+    assert content["detail"] == "Auth Token not found"
 
 
-def test_read_item_not_enough_permissions(
+def test_read_auth_token_not_enough_permissions(
     client: TestClient, normal_user_token_headers: dict[str, str], db: Session
 ) -> None:
-    item = create_random_item(db)
+    auth_token = create_random_auth_token(db)
     response = client.get(
-        f"{settings.API_V1_STR}/items/{item.id}",
+        f"{settings.API_V1_STR}/auth-tokens/{auth_token.id}",
         headers=normal_user_token_headers,
     )
     assert response.status_code == 400
@@ -65,13 +66,13 @@ def test_read_item_not_enough_permissions(
     assert content["detail"] == "Not enough permissions"
 
 
-def test_read_items(
+def test_read_auth_tokens(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
-    create_random_item(db)
-    create_random_item(db)
+    create_random_auth_token(db)
+    create_random_auth_token(db)
     response = client.get(
-        f"{settings.API_V1_STR}/items/",
+        f"{settings.API_V1_STR}/auth-tokens/",
         headers=superuser_token_headers,
     )
     assert response.status_code == 200
@@ -79,45 +80,44 @@ def test_read_items(
     assert len(content["data"]) >= 2
 
 
-def test_update_item(
+def test_update_auth_token(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
-    item = create_random_item(db)
-    data = {"title": "Updated title", "description": "Updated description"}
+    auth_token = create_random_auth_token(db)
+    data = {"tag_id": "TagId", "device_id": "DeviceId"}
     response = client.put(
-        f"{settings.API_V1_STR}/items/{item.id}",
+        f"{settings.API_V1_STR}/auth-tokens/{auth_token.id}",
         headers=superuser_token_headers,
         json=data,
     )
     assert response.status_code == 200
     content = response.json()
-    assert content["title"] == data["title"]
-    assert content["description"] == data["description"]
-    assert content["id"] == str(item.id)
-    assert content["owner_id"] == str(item.owner_id)
+    assert content["tag_id"] == data["tag_id"]
+    assert content["device_id"] == data["device_id"]
+    assert content["id"] == str(auth_token.id)
 
 
-def test_update_item_not_found(
+def test_update_auth_token_not_found(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
-    data = {"title": "Updated title", "description": "Updated description"}
+    data = {"tag_id": "TagId", "device_id": "DeviceId"}
     response = client.put(
-        f"{settings.API_V1_STR}/items/{uuid.uuid4()}",
+        f"{settings.API_V1_STR}/auth-tokens/{uuid.uuid4()}",
         headers=superuser_token_headers,
         json=data,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == "Item not found"
+    assert content["detail"] == "Auth Token not found"
 
 
-def test_update_item_not_enough_permissions(
+def test_update_auth_tokens_not_enough_permissions(
     client: TestClient, normal_user_token_headers: dict[str, str], db: Session
 ) -> None:
-    item = create_random_item(db)
-    data = {"title": "Updated title", "description": "Updated description"}
+    auth_token = create_random_auth_token(db)
+    data = {"tag_id": "TagId", "device_id": "DeviceId"}
     response = client.put(
-        f"{settings.API_V1_STR}/items/{item.id}",
+        f"{settings.API_V1_STR}/auth-tokens/{auth_token.id}",
         headers=normal_user_token_headers,
         json=data,
     )
@@ -126,37 +126,37 @@ def test_update_item_not_enough_permissions(
     assert content["detail"] == "Not enough permissions"
 
 
-def test_delete_item(
+def test_delete_auth_token(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
-    item = create_random_item(db)
+    auth_token = create_random_auth_token(db)
     response = client.delete(
-        f"{settings.API_V1_STR}/items/{item.id}",
+        f"{settings.API_V1_STR}/auth-tokens/{auth_token.id}",
         headers=superuser_token_headers,
     )
     assert response.status_code == 200
     content = response.json()
-    assert content["message"] == "Item deleted successfully"
+    assert content["message"] == "Auth Token deleted successfully"
 
 
-def test_delete_item_not_found(
+def test_delete_auth_token_not_found(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
     response = client.delete(
-        f"{settings.API_V1_STR}/items/{uuid.uuid4()}",
+        f"{settings.API_V1_STR}/auth-tokens/{uuid.uuid4()}",
         headers=superuser_token_headers,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == "Item not found"
+    assert content["detail"] == "Auth Token not found"
 
 
-def test_delete_item_not_enough_permissions(
+def test_delete_auth_token_not_enough_permissions(
     client: TestClient, normal_user_token_headers: dict[str, str], db: Session
 ) -> None:
-    item = create_random_item(db)
+    auth_token = create_random_auth_token(db)
     response = client.delete(
-        f"{settings.API_V1_STR}/items/{item.id}",
+        f"{settings.API_V1_STR}/auth-tokens/{auth_token.id}",
         headers=normal_user_token_headers,
     )
     assert response.status_code == 400
---
+++ yattt_data/backend/app/tests/utils/auth_token.py
@@ -0,0 +1,13 @@
+from sqlmodel import Session
+
+from app import crud
+from app.tests.utils.utils import random_lower_string
+
+from backend.app.models import AuthToken, AuthTokenCreate
+
+
+def create_random_auth_token(db: Session) -> AuthToken:
+    tag_id = random_lower_string()
+    device_id = random_lower_string()
+    auth_token_in = AuthTokenCreate(device_id=device_id, tag_id=tag_id)
+    return crud.create_auth_token(session=db, auth_token_in=auth_token_in)
